c2dfca954f9c96d7b47382444e9d0e32de59abae
ngmandyn <ngmandyn@sfu.ca>
2018-03-07 01:42:08 -0800

    Clean up and format my code a bit

--- js/modules/changeableColour.js
+++ js/modules/changeableColour.js
@@ -1,27 +1,27 @@
 function updateColours(data, value) {
   if (typeof(value) !== "undefined") {
 
-    var legendVals = d3.set(data.map( function(d) { return d[value]} ) ).values();
+    var legendVals = d3.set(data.map( function(d) { return d[value] } ) ).values();
 
-    if (value == 'Form factor') {
+    if (value === 'Form factor') {
       var colourScale = d3.scaleOrdinal(d3.schemeCategory10)
                           .domain(['Closed', 'Semi', 'Open'])
-    } else if (value == 'Amp required') {
+    } else if (value === 'Amp required') {
       colourScale = d3.scaleOrdinal(d3.schemeCategory20b)
                       .domain(['No', 'Maybe', 'Recommended', 'Yes'])
-    } else {
+    } else if (value === 'Manufacturer') {
       colourScale = d3.scaleOrdinal(d3.schemeCategory20)
                       .domain(legendVals);
     }
 
     var legendOrdinal = d3.legendColor()
-                          .shape("path", d3.symbol().type(d3.symbolCircle).size(100)())
+                          .shape('path', d3.symbol().type(d3.symbolCircle).size(100)())
                           .shapePadding(75)
-                          .cellFilter(function(d){ return d.label !== "e" })
+                          .cellFilter(function(d){ return d.label !== 'e' })
                           .scale(colourScale)
                           .orient('horizontal');
 
-    d3.select(".legendOrdinal")
+    d3.select('.legendOrdinal')
       .call(legendOrdinal);
 
     d3.selectAll('circle')
